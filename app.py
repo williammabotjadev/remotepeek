ddf59532cb5435d4d9c1baa6e40a4668a93f129b
williammabotjadev <williammabotjadev@gmail.com>
2021-04-02 16:49:47 +0200

    Routes and Links

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+@app.route('/login', methods=["GET", "POST"] )
+def login():
+    return render_template("login.html")
+
+@app.route('/signup', methods=["GET", "POST"] )
+def login():
+    return render_template("signup.html")
