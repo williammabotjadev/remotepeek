5fd96c906aa80e77d6fd26a18e6f08efeb2a35ca
williammabotjadev <williammabotjadev@gmail.com>
2021-04-02 18:27:24 +0200

    WTF Forms

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_wtf import request
 
 app = Flask(__name__)
 
@@ -8,6 +9,12 @@ def index():
 
 @app.route('/login', methods=["GET", "POST"] )
 def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
     return render_template("login.html")
 
 @app.route('/signup', methods=["GET", "POST"] )
