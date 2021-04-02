104cad95567a058850f7edfc3765d78f3519a85b
williammabotjadev <williammabotjadev@gmail.com>
2021-04-02 18:38:34 +0200

    revert

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_wtf import request
+from flask import Flask, render_template, request, url_for, redirect
+
 
 app = Flask(__name__)
 
@@ -9,12 +9,6 @@ def index():
 
 @app.route('/login', methods=["GET", "POST"] )
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('index'))
     return render_template("login.html")
 
 @app.route('/signup', methods=["GET", "POST"] )
