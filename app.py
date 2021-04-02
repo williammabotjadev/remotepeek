dd7ed2930e8ca30ba36d591fd256b1cc49d41ae9
williammabotjadev <williammabotjadev@gmail.com>
2021-04-02 16:53:30 +0200

    login function error fix

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/signin', methods=["GET", "POST"] )
-def signin():
+@app.route('/login', methods=["GET", "POST"] )
+def login():
     return render_template("login.html")
 
 @app.route('/signup', methods=["GET", "POST"] )
-def login():
+def signup():
     return render_template("signup.html")
 
 @app.route('/demo', methods=["GET", "POST"] )
-def login():
+def demo():
     return render_template("demo.html")
